5c3dd58a1cbbee8b257e00a53f9d06a61cdaf4f9
Peter Sarll <petersarll@gmail.com>
2019-07-14 23:01:25 -0700

    added functional app, just need to add instance locator, and key id and room id.

---
+++ message_app/src/Components/ChatApp.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
+import Input from './Input';
+import MessageList from './MessageList';
+
+class ChatApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null,
+            currentRoom: { users: [] },
+            messages: [],
+            users: []
+        }
+        this.addMessage = this.addMessage.bind(this);
+    }
+
+    componentDidMount() {
+        const chatManager = new ChatManager({
+            instanceLocator: "ADD_INSTANCE_LOCATOR_HERE",
+            userId: this.props.currentId,
+            tokenProvider: new TokenProvider({
+                url: "ADD_URL_HERE"
+            })
+        })
+
+        chatManager
+            .connect()
+            .then(currentUser => {
+                this.setState({ currentUser: currentUser })
+
+                return currentUser.subscribeToRoom({
+                    roomId: "ADD_ROOM_ID",
+                    messageLimit: 100,
+                    hooks: {
+                        onMessage: message => {
+                            this.setState({
+                                messages: [...this.state.messages, message],
+                            })
+                        },
+                    }
+                })
+            })
+            .then(currentRoom => {
+                this.setState({
+                    currentRoom,
+                    users: currentRoom.userIds
+                })
+            })
+            .catch(error => console.log(error))
+        }
+
+
+    addMessage(text) {
+        console.log(this.state);
+        this.state.currentUser.sendMessage({
+            text,
+            roomId: this.state.currentRoom.id
+        })
+            .catch(error => console.error('error', error));
+    }
+
+    render() {
+        return (
+            <div>
+                <h2 className="header">Let's Talk</h2>
+                <MessageList messages={this.state.messages} />
+                <Input className="input-field" onSubmit={this.addMessage} />
+            </div>
+        )
+    }
+}
+export default ChatApp;
